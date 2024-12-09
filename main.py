b613ffc96b29ed3847f562cf1e1d70b98a0b2502
Fabio Frazao <fsfrazao@gmail.com>
2024-12-09 09:56:46 -0400

    count the number of words in a book and print it

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
+def count_words(string):
+    return len(string.split())
 
 
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+
+    print(count_words(file_contents))
 
 main()
