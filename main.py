3f74fbc42692bfb3fd3f0dee3508ef99d8f3514c
Fabio Frazao <fsfrazao@gmail.com>
2024-12-09 09:58:14 -0400

    add 'get_character_frequency' function, to count the number of times each letter appears

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 
+def get_character_freq(string):
+    character_freq = {"a":0, "b":0, "c":0, "d":0, "e":0, "f":0, "g":0,
+                      "h":0, "i":0, "j":0, "k":0, "l":0, "m":0, "n":0,
+                      "o":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0,
+                      "v":0, "w":0, "y":0, "x":0, "z":0 }
+
+    string_lwr = string.lower()
+    for c in string_lwr:
+        if c in character_freq:
+            character_freq[c] += 1
+
+    return character_freq   
+
+
 def count_words(string):
     return len(string.split())
 
@@ -8,5 +22,6 @@ def main():
         file_contents = f.read()
 
     print(count_words(file_contents))
+    print(get_character_freq(file_contents))
 
 main()
